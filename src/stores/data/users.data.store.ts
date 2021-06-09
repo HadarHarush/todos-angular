738c46bf58ce8d3eec99991e0d6cc01b56589891
omergronich <gronichomer@gmail.com>
2021-06-09 20:46:17 +0300

    fixed reactivity bug

--- src/stores/data/users.data.store.ts
+++ src/stores/data/users.data.store.ts
@@ -1,10 +1,14 @@
-import { RootStore } from '../root.store';
-import { IStore } from '../types/store.interface';
+import { RootStore }  from '../root.store';
+import { IStore }     from '../types/store.interface';
+import { Injectable } from "@angular/core";
 
+@Injectable({providedIn: 'root'})
 export class UsersStore implements IStore {
-  currentUser: { id: number } = { id: 1 };
+  currentUser: { id: number } = {id: 1};
 
-  constructor(public root: RootStore) {}
+  constructor(public root: RootStore) {
+  }
 
-  async init() {}
+  async init() {
+  }
 }
